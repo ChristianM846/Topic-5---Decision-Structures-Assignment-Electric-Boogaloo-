816ffe61e0ca6f9cb2363882ade50f3e07d9a905
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-12 08:55:49 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Topic_5___Decision_Structures_Assignment__Electric_Boogaloo_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice;
+            bool done = false;
+
+            while (!done)
+            {
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+        }
+
+        public static void CompassBearings()
+        {
+            double angle;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

3c521a886d53c1eeae5b46d1c8d11df9de9f4e39
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-12 09:12:17 -0400

    In progress

--- Program.cs
+++ Program.cs
@@ -9,10 +9,31 @@
 
             while (!done)
             {
+                Console.WriteLine("Which program would you like to run?");
+                Console.WriteLine();
+                Console.WriteLine("1 - Compass Bearings");
+                Console.WriteLine("2 - Parking Garage Cost");
+                Console.WriteLine("3 - Hurricane");
+                Console.WriteLine();
+                Console.WriteLine("Input the number corresponding:");
 
+                while (!Int32.TryParse(Console.ReadLine().Trim(), out choice) || choice != 1 && choice != 2 && choice != 3)
+                {
+                    Console.WriteLine("That is not a valid input. Please input the number of the program you wish to run.");
+                }
 
-
-
+                if (choice == 1)
+                {
+                    CompassBearings();
+                }
+                else if (choice == 2)
+                {
+                    ParkingGarage();
+                }
+                else if(choice == 3)
+                {
+                    Hurricane();
+                }
 
 
 
@@ -27,12 +48,20 @@
 
         public static void CompassBearings()
         {
-            double angle;
+            double enteredAngle, actualAngel;
+
+
         }
 
+        public static void ParkingGarage()
+        {
 
+        }
 
+        public static void Hurricane()
+        {
 
+        }
 
 
 
